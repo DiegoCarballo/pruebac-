47e341ee688cd00c7f0c97fce2dc38c121182b49
Diego <diegohoraciocarballo@gmail.com>
2020-03-17 16:37:34 -0300

    Creacion de nuevo proyecto en C#

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Prueba_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Diego";
+            var age = 20;
+
+            if(age >= 21){
+
+                 Console.WriteLine(name);
+
+            }
+            else{
+                Console.WriteLine(" no es mayor de edad");
+            }
+
+           
+        }
+    }
+}
